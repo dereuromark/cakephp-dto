78d7f22f0335845f985d04e887085491403f0d84
mscherer <mark.scherer@spryker.com>
2019-02-28 12:14:08 +0100

    Add num assoc test

--- tests/TestCase/Dto/AssociativeTest.php
+++ tests/TestCase/Dto/AssociativeTest.php
@@ -22,6 +22,10 @@ class AssociativeTest extends TestCase {
 	}
 
 	/**
+	 * Shows PHP internal handling of string keys that are "numeric".
+	 *
+	 * They auto-transform to int array keys, as such careful with strict comparison afterwards.
+	 *
 	 * @return void
 	 */
 	public function testNumericKey() {
@@ -41,6 +45,43 @@ class AssociativeTest extends TestCase {
 			123 => '123456',
 		];
 		$this->assertSame($expected, $result);
+	}
+
+	/**
+	 * Tests that numeric values are OKish to be used as associative array keys.
+	 *
+	 * @return void
+	 */
+	public function testNumericMerge() {
+		$array = [
+			MutableMetaDto::FIELD_TITLE => 'blue',
+		];
+		$dto = new MutableMetaDto($array);
+
+		$metaValues = [
+			123 => '123a',
+			234 => '234a',
+			345 => '345a',
+		] + [
+			234 => '234b',
+			456 => '456b',
+		];
+		$this->assertSame('234a', $metaValues[234]);
+
+		$dto->addMetaValue(234, '234b');
+		$dto->addMetaValue(456, '456b');
+
+		$dto->addMetaValue(123, '123a');
+		$dto->addMetaValue(234, '234a');
+		$dto->addMetaValue(345, '345a');
+
+		$result = $dto->getMetaValue(234);
+		$this->assertSame('234a', $result);
+
+		$result = $dto->getMeta();
+		ksort($result);
+		ksort($metaValues);
+		$this->assertSame($metaValues, $result);
 	}
 
 }
