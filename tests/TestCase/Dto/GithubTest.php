5a23a768a9e30c59a789e209f13ebfecc74cf852
mscherer <mark.scherer@spryker.com>
2019-03-14 12:26:34 +0100

    Add github test.

---
+++ tests/TestCase/Dto/GithubTest.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace CakeDto\Test\TestCase\Dto;
+
+use CakeDto\Shell\DtoShell;
+use Cake\Console\ConsoleIo;
+use Cake\Event\EventManager;
+use Cake\TestSuite\TestCase;
+use Sandbox\Dto\Github\PullRequestDto;
+use TestApp\TestSuite\ConsoleOutput;
+use WyriHaximus\TwigView\Event\ExtensionsListener;
+use WyriHaximus\TwigView\Event\TokenParsersListener;
+
+class GithubTest extends TestCase {
+
+	/**
+	 * @var \CakeDto\Shell\DtoShell|\PHPUnit\Framework\MockObject\MockObject
+	 */
+	protected $shell;
+
+	/**
+	 * @return void
+	 */
+	public function testMapping() {
+		$this->skipIf(version_compare(PHP_VERSION, '7.1') < 0, 'Requires PHP 7.1+');
+
+		$file = TESTS . 'files' . DS . 'Github' . DS . 'demo_pr.json';
+
+		$simulatedDataFromGitHubApi = json_decode(file_get_contents($file), true);
+
+		EventManager::instance()->on(new ExtensionsListener());
+		EventManager::instance()->on(new TokenParsersListener());
+
+		$this->out = new ConsoleOutput();
+		$this->err = new ConsoleOutput();
+		$io = new ConsoleIo($this->out, $this->err);
+		$this->shell = $this->getMockBuilder(DtoShell::class)->setMethods(['_getConfigPath', '_getSrcPath'])->setConstructorArgs([$io])->getMock();
+
+		$sandboxPluginPath = TESTS . 'test_app' . DS . 'plugins' . DS . 'Sandbox' . DS;
+
+		$this->shell->expects($this->any())->method('_getConfigPath')->willReturn($sandboxPluginPath . 'config' . DS);
+		$this->shell->expects($this->any())->method('_getSrcPath')->willReturn($sandboxPluginPath . 'src' . DS);
+
+		$result = $this->shell->runCommand(['generate', '-v', '-p', 'Sandbox', '-d']);
+		$this->assertSame(0, $result, 'Code: ' . $result . ' (expected 0, no change). Remove -d and re-run.');
+
+		$pullRequestDto = PullRequestDto::create($simulatedDataFromGitHubApi, true, PullRequestDto::TYPE_UNDERSCORED);
+
+		$pullRequestDtoArray = $pullRequestDto->toArray();
+
+		$expected = [
+			'url' => 'https://api.github.com/repos/octocat/Hello-World/pulls/1347',
+			'number' => 1347,
+			'state' => 'open',
+			'title' => 'new-feature',
+			'body' => 'Please pull these awesome changes',
+			'user' =>
+				 [
+					'login' => 'octocat',
+					'html_url' => 'https://github.com/octocat',
+					'type' => 'User',
+				],
+			'createdAt' => '2011-01-26T19:01:12Z',
+			'labels' =>
+				 [
+					'bug' =>
+						 [
+							'name' => 'bug',
+							'color' => 'f29513',
+						],
+				],
+			'head' =>
+				 [
+					'ref' => 'new-topic',
+					'sha' => '6dcb09b5b57875f334f61aebed695e2e4193db5e',
+					'user' =>
+						 [
+							'login' => 'octocat',
+							'html_url' => 'https://github.com/octocat',
+							'type' => 'User',
+						],
+					'repo' =>
+						 [
+							'name' => 'Hello-World',
+							'html_url' => 'https://github.com/octocat/Hello-World',
+							'private' => false,
+							'owner' =>
+								 [
+									'login' => 'octocat',
+									'html_url' => 'https://github.com/octocat',
+									'type' => 'User',
+								],
+						],
+				],
+			'base' =>
+				 [
+					'ref' => 'master',
+					'sha' => '6dcb09b5b57875f334f61aebed695e2e4193db5e',
+					'user' =>
+						 [
+							'login' => 'octocat',
+							'html_url' => 'https://github.com/octocat',
+							'type' => 'User',
+						],
+					'repo' =>
+						 [
+							'name' => 'Hello-World',
+							'html_url' => 'https://github.com/octocat/Hello-World',
+							'private' => false,
+							'owner' =>
+								 [
+									'login' => 'octocat',
+									'html_url' => 'https://github.com/octocat',
+									'type' => 'User',
+								],
+						],
+				],
+		];
+
+		$this->assertSame($expected, $pullRequestDtoArray);
+	}
+
+}
---
+++ tests/test_app/plugins/Sandbox/src/Dto/Jira/IssueDto.php
@@ -0,0 +1,329 @@
+<?php
+/**
+ * !!! Auto generated file. Do not directly modify this file. !!!
+ * You can either version control this or generate the file on the fly prior to usage/deployment.
+ */
+
+namespace Sandbox\Dto\Jira;
+
+/**
+ * Jira/Issue DTO
+ *
+ * @property string $id
+ * @property string $key
+ * @property string $status
+ * @property string $priority
+ * @property string $summary
+ * @property string|null $version
+ */
+class IssueDto extends \CakeDto\Dto\AbstractDto {
+
+	const FIELD_ID = 'id';
+	const FIELD_KEY = 'key';
+	const FIELD_STATUS = 'status';
+	const FIELD_PRIORITY = 'priority';
+	const FIELD_SUMMARY = 'summary';
+	const FIELD_VERSION = 'version';
+
+	/**
+	 * @var string
+	 */
+	protected $id;
+
+	/**
+	 * @var string
+	 */
+	protected $key;
+
+	/**
+	 * @var string
+	 */
+	protected $status;
+
+	/**
+	 * @var string
+	 */
+	protected $priority;
+
+	/**
+	 * @var string
+	 */
+	protected $summary;
+
+	/**
+	 * @var string|null
+	 */
+	protected $version;
+
+	/**
+	 * Some data is only for debugging for now.
+	 *
+	 * @var array
+	 */
+	protected $_metadata = [
+		'id' => [
+			'name' => 'id',
+			'type' => 'string',
+			'required' => true,
+			'defaultValue' => null,
+			'dto' => null,
+			'collectionType' => null,
+			'associative' => false,
+			'key' => null,
+			'serializable' => false,
+			'toArray' => false,
+		],
+		'key' => [
+			'name' => 'key',
+			'type' => 'string',
+			'required' => true,
+			'defaultValue' => null,
+			'dto' => null,
+			'collectionType' => null,
+			'associative' => false,
+			'key' => null,
+			'serializable' => false,
+			'toArray' => false,
+		],
+		'status' => [
+			'name' => 'status',
+			'type' => 'string',
+			'required' => true,
+			'defaultValue' => null,
+			'dto' => null,
+			'collectionType' => null,
+			'associative' => false,
+			'key' => null,
+			'serializable' => false,
+			'toArray' => false,
+		],
+		'priority' => [
+			'name' => 'priority',
+			'type' => 'string',
+			'required' => true,
+			'defaultValue' => null,
+			'dto' => null,
+			'collectionType' => null,
+			'associative' => false,
+			'key' => null,
+			'serializable' => false,
+			'toArray' => false,
+		],
+		'summary' => [
+			'name' => 'summary',
+			'type' => 'string',
+			'required' => true,
+			'defaultValue' => null,
+			'dto' => null,
+			'collectionType' => null,
+			'associative' => false,
+			'key' => null,
+			'serializable' => false,
+			'toArray' => false,
+		],
+		'version' => [
+			'name' => 'version',
+			'type' => 'string',
+			'required' => false,
+			'defaultValue' => null,
+			'dto' => null,
+			'collectionType' => null,
+			'associative' => false,
+			'key' => null,
+			'serializable' => false,
+			'toArray' => false,
+		],
+	];
+
+	/**
+	* @var array
+	*/
+	protected $_keyMap = [
+		'underscored' => [
+			'id' => 'id',
+			'key' => 'key',
+			'status' => 'status',
+			'priority' => 'priority',
+			'summary' => 'summary',
+			'version' => 'version',
+		],
+		'dashed' => [
+			'id' => 'id',
+			'key' => 'key',
+			'status' => 'status',
+			'priority' => 'priority',
+			'summary' => 'summary',
+			'version' => 'version',
+		],
+	];
+
+	/**
+	 * @param string $id
+	 *
+	 * @return $this
+	 */
+	public function setId(string $id) {
+		$this->id = $id;
+		$this->_touchedFields[self::FIELD_ID] = true;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getId(): string {
+		return $this->id;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasId() {
+		return $this->id !== null;
+	}
+
+	/**
+	 * @param string $key
+	 *
+	 * @return $this
+	 */
+	public function setKey(string $key) {
+		$this->key = $key;
+		$this->_touchedFields[self::FIELD_KEY] = true;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getKey(): string {
+		return $this->key;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasKey() {
+		return $this->key !== null;
+	}
+
+	/**
+	 * @param string $status
+	 *
+	 * @return $this
+	 */
+	public function setStatus(string $status) {
+		$this->status = $status;
+		$this->_touchedFields[self::FIELD_STATUS] = true;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getStatus(): string {
+		return $this->status;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasStatus() {
+		return $this->status !== null;
+	}
+
+	/**
+	 * @param string $priority
+	 *
+	 * @return $this
+	 */
+	public function setPriority(string $priority) {
+		$this->priority = $priority;
+		$this->_touchedFields[self::FIELD_PRIORITY] = true;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPriority(): string {
+		return $this->priority;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasPriority() {
+		return $this->priority !== null;
+	}
+
+	/**
+	 * @param string $summary
+	 *
+	 * @return $this
+	 */
+	public function setSummary(string $summary) {
+		$this->summary = $summary;
+		$this->_touchedFields[self::FIELD_SUMMARY] = true;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSummary(): string {
+		return $this->summary;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasSummary() {
+		return $this->summary !== null;
+	}
+
+	/**
+	 * @param string|null $version
+	 *
+	 * @return $this
+	 */
+	public function setVersion(?string $version = null) {
+		$this->version = $version;
+		$this->_touchedFields[self::FIELD_VERSION] = true;
+
+		return $this;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getVersion(): ?string {
+		return $this->version;
+	}
+
+	/**
+	 * @throws \RuntimeException If value is not set.
+	 *
+	 * @return string
+	 */
+	public function getVersionOrFail(): string {
+		if (!isset($this->version)) {
+			throw new \RuntimeException('Value not set for field `version` (expected to be not null)');
+		}
+
+		return $this->version;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasVersion() {
+		return $this->version !== null;
+	}
+
+}
