c5175e23a5f3f745c23ebdf60aec5476269f58b5
mscherer <mark.scherer@spryker.com>
2020-03-25 03:14:30 +0100

    Remove auto-reload

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,5 +3,5 @@ use Cake\Event\EventManager;
 use Cake\TwigView\Event\ExtensionsListener;
 use Cake\TwigView\Event\TokenParsersListener;
 
-EventManager::instance()->on(new ExtensionsListener());
-EventManager::instance()->on(new TokenParsersListener());
+//EventManager::instance()->on(new ExtensionsListener());
+//EventManager::instance()->on(new TokenParsersListener());
