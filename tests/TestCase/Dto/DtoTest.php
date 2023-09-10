9c7c3fb87e54b1a4e2e882ea5da15eacea8d3538
mscherer <dereuromark@web.de>
2023-09-10 16:44:19 +0200

    Fix tests.

--- tests/TestCase/Dto/DtoTest.php
+++ tests/TestCase/Dto/DtoTest.php
@@ -513,7 +513,7 @@ class DtoTest extends TestCase {
 		$array = [
 			TransactionDto::FIELD_VALUE => 33,
 			TransactionDto::FIELD_CUSTOMER_ACCOUNT => $customerAccount,
-			TransactionDto::FIELD_CREATED => FrozenDate::now(),
+			TransactionDto::FIELD_CREATED => (new FrozenDate())->now(),
 		];
 
 		$dto = new TransactionDto($array);
@@ -532,7 +532,7 @@ class DtoTest extends TestCase {
 		$array = [
 			TransactionDto::FIELD_VALUE => '33.0',
 			TransactionDto::FIELD_CUSTOMER_ACCOUNT => $customerAccount,
-			TransactionDto::FIELD_CREATED => FrozenDate::now(),
+			TransactionDto::FIELD_CREATED => (new FrozenDate())->now(),
 		];
 
 		$e = null;
