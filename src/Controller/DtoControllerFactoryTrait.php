8e850ce624643624fc1bc0ee7f1bd8bee3ebb7ad
mscherer <dereuromark@web.de>
2026-02-04 16:16:48 +0100

    Fix controller factory trait type

--- src/Controller/DtoControllerFactoryTrait.php
+++ src/Controller/DtoControllerFactoryTrait.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace CakeDto\Controller;
 
-use Cake\Controller\ControllerFactoryInterface;
+use Cake\Http\ControllerFactoryInterface;
 
 trait DtoControllerFactoryTrait {
 
 	/**
-	 * @return \Cake\Controller\ControllerFactoryInterface
+	 * @return \Cake\Http\ControllerFactoryInterface
 	 */
 	public function controllerFactory(): ControllerFactoryInterface {
 		return new DtoControllerFactory($this->getContainer());
